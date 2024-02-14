78cea126c0c6bafb3e23d93b556ed5abfc73c98d
Kiiingdom <video.boy@hotmail.co.uk>
2024-02-14 03:36:15 -0400

    Hopefully final commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,6 @@
 @section('content')
  <h2>Dominic Olukoga</h2>
  <p>Dom The Don here the one and only. Tired and working tirelessly on your behalf. <br> <b>NOTE:</b> Not a politician</p>
+ <img src="{{ url('/images/WhatsApp Image 2023-09-26 at 07.22.56.jpeg') }}" alt="The SoSCAI Guild Committee Logo. Felt fitting idk. " /> 
+
 @endsection
